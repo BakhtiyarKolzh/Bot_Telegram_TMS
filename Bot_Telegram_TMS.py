a895a6c418af44da3c1ec7b92ade02b64c8f1272
B.Tokmyrza <b.koljabai@taimas-group.kz>
2022-08-11 11:17:04 +0600

    /11/08  2022-08-11

--- Bot_Telegram_TMS.py
+++ Bot_Telegram_TMS.py
@@ -8,18 +8,30 @@ from keyboard import press
 import telebot
 from telebot import types
 
+#    My libraries
 import configure  #### Library for Token
 import authentication   #### Library Protect ID
 
-#################################
-#     Input dates
-bot = telebot.TeleBot(configure.config["token"])
-users_start = [315207431,385753167]  # последнее - id группы если бот что-то должен делать в группе
 
-#################################
+
+#                                       INPUT DATES
+##############################################################################
+bot = telebot.TeleBot(configure.config["token"])
+users_start = authentication.config["ID"]            # последнее - id группы если бот что-то должен делать в группе
+
+
+url_BIM360 = "https://insight.b360.eu.autodesk.com/accounts/bf8a62b2-d479-4c2e-8523-103a1de299ea/projects/7a15c326-d421-4efb-98cd-fa817eb95f96/home"
+url_Google_Sheets = "https://docs.google.com/spreadsheets/d/1tbvsFXLMzuKftQu9LV9jQ4FD6ik_l5yP-XRrhQsCrvw/edit?usp=sharing"
+url_Yandex_Disk = "https://disk.yandex.kz/client/disk?utm_source=main_stripe_big_more"
+url_BI_Design = "https://design.bi.group/"
+url_Google_Docs = "https://docs.google.com/spreadsheets/d/1WesHLNRMiR5OOTFm0-t-VFiDfdix1NWCtgoqZq52nFI/edit#gid=0"
 
 ##############################################################################
-# Органичение выполнение команды start
+
+
+
+#                                    --Bot Protection---
+
 @bot.message_handler(func=lambda message: message.chat.id not in users_start, commands=['start', "print", "url"])
 def some(message):
     bot.send_message(message.chat.id, 'У Вас нет прав на выполнение данной команды, обратитесь к администратору')
@@ -27,7 +39,7 @@ def some(message):
 
 ##############################################################################
 
-
+#                                   ----start Telegram bot----
 ##############################################################################
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -44,6 +56,9 @@ def start(message):
 
 ##############################################################################   Button for process /print
 
+
+#                                 def for -print-   Creating buttons
+##############################################################################
 @bot.message_handler(commands=['print'])
 def export_par(message):
     markup = types.InlineKeyboardMarkup(row_width=3)
@@ -52,7 +67,6 @@ def export_par(message):
     button_PDF = types.InlineKeyboardButton('PDF', callback_data="PDF")
     markup.add(button_DWG, button_NWC, button_PDF)
     bot.send_message(message.chat.id, "Выберите формат для перевода данных:", reply_markup=markup)
-
     return
 
 @bot.callback_query_handler(func=lambda call: True)
@@ -74,61 +88,45 @@ def callback(call):
 ##################################################################################################################
 
 
+#                                DWG CONVERTER
 ###############################################################################
 def user_answer_for_DWG(message):
     open_dir = str(message.text)
     if os.path.exists(open_dir):
+
+        #  SCAN DIR FOR REVIT
+        # listPaths = SortRevitNameFiles()
+
         filepath = os.path.join(open_dir + "\ExportToDWG.bat")
         PathLaunch(filepath, message, "DWG")
         print("DWG")
+        result = bot.send_message(message.chat.id, "Выберите файлы:")
+        bot.register_next_step_handler(result, func_for_keyboard)
     else:
         bot.send_message(message.chat.id, "ОШИБКА ПУТИ, ВВЕДИТЕ ПУТЬ ЗАНОВО!!!")
     return
-
+###############################################################################
+#                                NWC CONVERTER
+###############################################################################
 
 def user_answer_for_NWC(message):
     open_dir = str(message.text)
     if os.path.exists(open_dir):
+
+        #  SCAN DIR FOR REVIT
+        # listPaths = SortRevitNameFiles()
+
         filepath = os.path.join(open_dir + "\ExportToNavisworks.bat")
         PathLaunch(filepath, message, "NWC")
         print("NWC")
+        result = bot.send_message(message.chat.id, "Выберите файлы:")
+        bot.register_next_step_handler(result, func_for_keyboard)
     else:
         bot.send_message(message.chat.id, "ОШИБКА ПУТИ, ВВЕДИТЕ ПУТЬ ЗАНОВО!!!")
     return
 
-
-# def user_answer_for_PDF(message):
-#     open_dir = str(message.text)
-#     if os.path.exists(open_dir):
-#         filepath = os.path.join(open_dir + "\ExportToPDF.bat")
-#         PathLaunch(filepath, message, "PDF")
-#         print("PDF")
-#     else:
-#         bot.send_message(message.chat.id, "ОШИБКА ПУТИ, ВВЕДИТЕ ПУТЬ ЗАНОВО!!!")
 ##################################################################################################################
-
-
-##############################################################################################  Fuction for call
-def PathLaunch(filepath, message, call):
-    if (os.path.exists(filepath)):
-        # os.startfile(filepath)
-        print("Старт")
-        bot.send_message(message.chat.id, f"Процесс выгрузки в {call} запущен")
-    return
-#################################################################################################################
-
-
-
-################################################################
-# def SortRevitNameFiles():
-#     return
-#################################################################
-
-
-
-
-
-
+#                                PDF CONVERTER
 #####################################################################################################
 def user_answer_for_PDF(message):
     open_dir = str(message.text)
@@ -145,11 +143,32 @@ def user_answer_for_PDF(message):
 
     else:
         bot.send_message(message.chat.id, "ОШИБКА ПУТИ, ВВЕДИТЕ ПУТЬ ЗАНОВО!!!")
-
     return
 
 ###############################################################################################################
 
+#                                -DEF- FOR start file
+##############################################################################################
+def PathLaunch(filepath, message, call):
+    if (os.path.exists(filepath)):
+        os.startfile(filepath)
+        print("Старт")
+        bot.send_message(message.chat.id, f"Процесс выгрузки в {call} запущен")
+    return
+#################################################################################################################
+
+#                                -DEF- TO Sort files
+################################################################
+# def SortRevitNameFiles():
+#     return
+#################################################################
+
+
+
+
+
+
+#                                -DEF- FOR Keyboard control
 ###########################################################################################################
 def func_for_keyboard(message):
     number_of_file = str(message.text)
@@ -162,25 +181,19 @@ def func_for_keyboard(message):
         keyboard.write("0")
         press('enter')
         bot.send_message(message.chat.id, f"Операция по выгрузке запущена")
+    elif number_of_file == "-":
+        keyboard.write("-")
+        press('enter')
+        bot.send_message(message.chat.id, f"Выполнена отмена операции по запуску")
     else:
         console_b = bot.send_message(message.chat.id, "ОШИБКА ВВОДА!!!")
         bot.register_next_step_handler(console_b, func_for_keyboard)
-
     return
 
 #############################################################################################################
 
-
-
-
-### #######################################     URL for BOT       #################################################
-
-url_BIM360 = "https://insight.b360.eu.autodesk.com/accounts/bf8a62b2-d479-4c2e-8523-103a1de299ea/projects/7a15c326-d421-4efb-98cd-fa817eb95f96/home"
-url_Google_Sheets = "https://docs.google.com/spreadsheets/d/1tbvsFXLMzuKftQu9LV9jQ4FD6ik_l5yP-XRrhQsCrvw/edit?usp=sharing"
-url_Yandex_Disk = "https://disk.yandex.kz/client/disk?utm_source=main_stripe_big_more"
-url_BI_Design = "https://design.bi.group/"
-url_Google_Docs = "https://docs.google.com/spreadsheets/d/1WesHLNRMiR5OOTFm0-t-VFiDfdix1NWCtgoqZq52nFI/edit#gid=0"
-##################################################################################################################################
+#                                BUTTON FOR -url-
+####################################################################################################################
 @bot.message_handler(commands=['url'])
 def website(message):
     markup = types.InlineKeyboardMarkup(row_width=3)
@@ -192,90 +205,16 @@ def website(message):
     markup.add(button_for_bim360, button_for_Google_Sheets, button_for_BI_Design, button_for_Yandex_Disk,
                button_for_Google_Docs)
     bot.send_message(message.chat.id, "Пройдите по ссылке снизу:", reply_markup=markup)
-
     return
+#####################################################################################################################
 
-####################################################################################################################################
+
+
+
+
+#-------------------------------------------OUT --------------------
 
 bot.polling(none_stop=True)
 #######################################################################################################
 
-
-# It needs for save, because maybe mistakes##################
-###Fuck you
-
-# open_dir = message.text
-#     open_dir=str(open_dir)
-#     for bat_name in os.listdir(open_dir):
-#         filepath = os.path.join(open_dir + "\ExportToDWG.bat")
-#         if (os.path.exists(filepath)):
-#             os.startfile(filepath)
-#             bot.send_message(message.chat.id, "Процесс выгрузки в DWG запущен")
-#             return
-#############################################################
-# def user_answer_for_DWG(message):  ###   open bat file and show on the desktop
-#     open_dir = str(message.text)
-#     if os.path.exists(open_dir):
-#         filepath = os.path.join(open_dir + "\ExportToDWG.bat")
-#         if (os.path.exists(filepath)):
-#             os.startfile(filepath)
-#             bot.send_message(message.chat.id, "Процесс выгрузки в DWG запущен, ожидайте меню для выбора файлов")
-#
-#         return
-#     else:
-#         dir_for_DWG = bot.send_message(message.chat.id, "ОШИБКА ПУТИ, ВВЕДИТЕ ПУТЬ ЗАНОВО!!!")
-#         bot.register_next_step_handler(dir_for_DWG, user_answer_for_DWG)
-
-
-#################################################                                        Experiment with NWC format
-
-
-# def user_answer_for_NWC(message):
-#     open_dir = str(message.text)
-#     if os.path.exists(open_dir):
-#         filepath = os.path.join(open_dir + "\ExportToNavisworks.bat")
-#         if (os.path.exists(filepath)):
-#             os.startfile(filepath)
-#             bot.send_message(message.chat.id, "Процесс выгрузки в NWC запущен, ожидайте меню для выбора файлов")
-#
-#     else:
-#         dir_for_NWC=bot.send_message(message.chat.id, "ОШИБКА ПУТИ, ВВЕДИТЕ ПУТЬ ЗАНОВО!!!")
-#         bot.register_next_step_handler(dir_for_NWC, user_answer_for_NWC)
-#     return
-
-
-# def user_answer_for_PDF(message):
-#     open_dir = str(message.text)
-#     if os.path.exists(open_dir):
-#         for file_name in os.listdir(open_dir):
-#             if fnmatch.fnmatch(file_name, '*.rvt'):
-#                 print(file_name)
-#                 bot.send_message(message.chat.id, file_name)
-# ##  end PROCESS
-#         filepath = os.path.join(open_dir + "\ExportToPDF.bat")
-#         PathLaunch(filepath, message, "PDF")
-#         print("PDF")
-#         result=bot.send_message(message.chat.id, "Выберите файлы:")
-#         bot.register_next_step_handler(result, func_for_keyboard)
-#
-#
-#
-#     else:
-#         bot.send_message(message.chat.id, "ОШИБКА ПУТИ, ВВЕДИТЕ ПУТЬ ЗАНОВО!!!")
-
-#
-# #
-# def func_for_keyboard(message):
-#     number_of_file = str(message.text)
-#     if number_of_file >"0":
-#         keyboard.write(number_of_file)
-#         press('enter')
-#         console_a = bot.send_message(message.chat.id, f"Выбана секция {number_of_file}")
-#         bot.register_next_step_handler(console_a, func_for_keyboard)
-#     elif number_of_file == "0":
-#         keyboard.write("0")
-#         press('enter')
-#         bot.send_message(message.chat.id, f"Операция по выгрузке запущена")
-#     else:
-#         console_b=bot.send_message(message.chat.id, "ОШИБКА ВВОДА!!!")
-#         bot.register_next_step_handler(console_b, func_for_keyboard)
+#                                          -SAVES-
--- authentication.py
+++ authentication.py
@@ -1,18 +1,9 @@
 
-# Органичение выполнение команды start
-
 config ={
-    'name': "@zserdxbot",
-    "token": "5357562508:AAGRmz_kvhoYqV0ZpBgxufOWOxgQQ5ITrDI",
-    'url': f'https://git.heroku.com/telegram-tms-bot.git{"token"}'
+    'ID':[385753167,315207431]
 }
 
 
 
-users_start = [315207431,385753167]  # последнее - id группы если бот что-то должен делать в группе
 
-@bot.message_handler(func=lambda message: message.chat.id not in users_start, commands=['start', "print", "url"])
-def some(message):
-    bot.send_message(message.chat.id, 'У Вас нет прав на выполнение данной команды, обратитесь к администратору')
-    return
 
