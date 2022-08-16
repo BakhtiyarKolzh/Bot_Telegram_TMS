c4af6c96773a060a2dc997b52a1811e08dc837fe
B.Tokmyrza <b.koljabai@taimas-group.kz>
2022-08-16 11:17:08 +0600

    /11/16 2022/08/16(кнопки кроме обратного вызова инпут пдф и ссылки)

--- TelegramBot.py
+++ TelegramBot.py
@@ -10,7 +10,6 @@ from telebot import types
 import configure  #### Library for Token
 import authentication   #### Library Protect ID
 import RevitSortFiles
-import Callbutton  #### Library for button
 
 
 
@@ -33,18 +32,17 @@ url_Google_Docs = "https://docs.google.com/spreadsheets/d/1WesHLNRMiR5OOTFm0-t-V
 #                                    --Bot Protection---
 
 @bot.message_handler(func=lambda message: message.chat.id not in users_start, commands=['start', "url"])
-def BotProtection(message):
+def protection_id(message):
     bot.send_message(message.chat.id, 'У Вас нет прав на выполнение данной команды, обратитесь к администратору')
     return
 
 ########################################################################################################################
 
-
 '''   START TELEGRAM    '''
 
 ########################################################################################################################
 @bot.message_handler(commands=['start'])
-def Start(message):
+def start(message):
     welcome = f"Добро пожаловать, <b>{message.from_user.first_name}</b>"
     bot.send_message(message.chat.id, welcome, parse_mode='html')
 
@@ -59,21 +57,28 @@ def Start(message):
 
 
 @bot.message_handler(content_types=['text'])
-def StartTask(message):
+def start_task(message):
     if message.chat.type == "private":
         if message.text == "Начать задание":
-            Call_button_batch(message)
+            call_button_batch(message)
 
     return
+
+# @bot.message_handler(content_types=['text'])
+# def start_url(message):
+#     if message.chat.type == "private":
+#         if message.text == "URL":
+#             Website(message)
+#
+#     return
 
 ########################################################################################################################
 
 '''   CREATE BUTTON ---- DWG,NWC,PDF   '''
-########################################################################################################################
-########################################################################################################################
+#######################################################################################################################
 
 @bot.message_handler(content_types=['text'])
-def Call_button_batch(message):
+def call_button_batch(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     button_DWG = types.KeyboardButton('DWG')
     button_NWC =types.KeyboardButton('NWC')
@@ -82,23 +87,23 @@ def Call_button_batch(message):
     markup.add(button_DWG, button_NWC, button_PDF)
 
     result=bot.send_message(message.chat.id, "Выберите формат для перевода данных:", reply_markup=markup)
-    bot.register_next_step_handler(result, Callback)
+    bot.register_next_step_handler(result, callback)
 
     return
 
 @bot.message_handler(content_types=['text'])
-def Callback(message):
+def callback(message):
     if message.text == "DWG":
         dir_for_DWG=bot.send_message(message.chat.id, "Введите путь для DWG")
-        bot.register_next_step_handler(dir_for_DWG, User_answer_for_DWG)
+        bot.register_next_step_handler(dir_for_DWG, user_answer_for_DWG)
 
     elif message.text == "NWC":
         dir_for_NWC = bot.send_message(message.chat.id, "Введите путь для NWC")
-        bot.register_next_step_handler(dir_for_NWC, User_answer_for_NWC)
+        bot.register_next_step_handler(dir_for_NWC, user_answer_for_NWC)
 
     elif message.text == "PDF":
         dir_for_PDF = bot.send_message(message.chat.id, 'Введите путь для PDF')
-        bot.register_next_step_handler(dir_for_PDF, User_answer_for_PDF)
+        bot.register_next_step_handler(dir_for_PDF, user_answer_for_PDF)
 
     return
 ########################################################################################################################
@@ -108,15 +113,10 @@ def Callback(message):
 #########
 #########
 
-         ###############################################################################################################
-         ###############################################################################################################
-
-########################################################################################################################
-
 '''DWG CONVERTER'''
 
 ########################################################################################################################
-def User_answer_for_DWG(message):
+def user_answer_for_DWG(message):
     open_dir = str(message.text)
     if os.path.exists(open_dir):
 
@@ -124,7 +124,7 @@ def User_answer_for_DWG(message):
         # listPaths = SortRevitNameFiles()
 
         filepath = os.path.join(open_dir + "\ExportToDWG.bat")
-        PathLaunch(filepath, message, "DWG")
+        path_launch(filepath, message, "DWG")
         print("DWG")
         result = bot.send_message(message.chat.id, "Выберите файлы:")
         # bot.register_next_step_handler(result, func_for_keyboard)
@@ -137,7 +137,7 @@ def User_answer_for_DWG(message):
 
 ########################################################################################################################
 
-def User_answer_for_NWC(message):
+def user_answer_for_NWC(message):
     open_dir = str(message.text)
 
     if os.path.exists(open_dir):
@@ -147,7 +147,7 @@ def User_answer_for_NWC(message):
         # listPaths = SortRevitNameFiles()
 
         filepath = os.path.join(open_dir + "\ExportToNavisworks.bat")
-        PathLaunch(filepath, message, "NWC")
+        path_launch(filepath, message, "NWC")
         print("NWC")
         result = bot.send_message(message.chat.id, "Выберите файлы:")
         # bot.register_next_step_handler(result, func_for_keyboard)
@@ -158,7 +158,8 @@ def User_answer_for_NWC(message):
 ########################################################################################################################
 '''PDF CONVERTER'''
 
-def User_answer_for_PDF(message):
+
+def user_answer_for_PDF(message):
     input_path = os.path.realpath(message.text)
     if os.path.exists(input_path):
         print(input_path)
@@ -170,9 +171,9 @@ def User_answer_for_PDF(message):
 
         filepath = os.path.join(input_path + "\ExportToPDF.bat")
 
-        PathLaunch(filepath, message, "PDF")
+        path_launch(filepath, message, "PDF")
         print("PDF")
-        Menu_for_button(message)
+        menu_for_button(message)
 
         # bot.register_next_step_handler(result, new_func)
 
@@ -180,39 +181,46 @@ def User_answer_for_PDF(message):
         bot.send_message(message.chat.id, "ОШИБКА ПУТИ, ВВЕДИТЕ ПУТЬ ЗАНОВО!!!")
     return
 
-##################
+
+########################################################################################################################
 ########################################################################################################################
 
-'''Start def'''
+#########
+#########
+#########
 
-def PathLaunch(filepath, message, call):
+'''Start def'''
+########################################################################################################################
+
+def path_launch(filepath, message, call):
     if (os.path.exists(filepath)):
         # os.startfile(filepath)
         print("Старт")
         bot.send_message(message.chat.id, f"Процесс выгрузки в {call} запущен")
     return
+
 ########################################################################################################################
 ########################################################################################################################
+
 '''SELECT A SECTION --- def'''
 
 def new_func(message):
     number_of_file = str(message.text)
     print(number_of_file)
-    Call_button_ok(message)
+    call_button_ok(message)
     # result = bot.send_message(message.chat.id, f"Выберана секция {number_of_file}")
     # bot.register_next_step_handler(result, Call_button_ok)
     return
 
 
-
-
 ########################################################################################################################
 ########################################################################################################################
+
 '''Menu_for_button'''
 
 
 @bot.message_handler(content_types=['text'])
-def Menu_for_button(message):
+def menu_for_button(message):
 
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
 
@@ -223,90 +231,94 @@ def Menu_for_button(message):
     markup.add(button_select_files, button_all_files, button_close)
 
     result=bot.send_message(message.chat.id, "Выберите нужную операцию", reply_markup=markup)
-    bot.register_next_step_handler(result, Select_button)
+    bot.register_next_step_handler(result, select_button)
 
     return
 
-def Select_button(message):
+def select_button(message):
     if message.text == "Выбор файлов":
         bot.send_message(message.chat.id, "СПИСОК ФАЙЛОВ ПОДАН, ВЫБЕРИТЕ НЕОБХОДИМЫЕ")
-        Call_button_ok(message)
+        call_button_ok(message)    ###### переместить в функицю по выбору файлов
 
 
     elif message.text == "Выбрать все файлы":
-        result = bot.send_message(message.chat.id, "ВСЕ ФАЙЛЫ ВЫБРАНЫ")
-        Start(message)     #####   нужна директория и функция для выбора всех файлов
+        bot.send_message(message.chat.id, "ВСЕ ФАЙЛЫ ВЫБРАНЫ")
+        start(message)     #####   нужна директория и функция для выбора всех файлов
 
     elif message.text == "ОТМЕНА":
         bot.send_message(message.chat.id, " ОПЕРАЦИЯ ОТМЕНЕНА")
-        Start(message)
+        start(message)
 
 
     return
 ########################################################################################################################
 ########################################################################################################################
+
+'''kEYBOARD OK'''
+
 @bot.message_handler(content_types=['text'])
-def Call_button_ok(message):
+def call_button_ok(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     button_name_ok = types.KeyboardButton('ОК')
     button_name_close = types.KeyboardButton('ОТМЕНА')
     markup.add(button_name_ok,button_name_close)
 
-    result=bot.send_message(message.chat.id, "Подтвердите операцию нажав на кнопку ОК, если нет Отмена", reply_markup=markup)
-    bot.register_next_step_handler(result, Button_ok)
-
+    result=bot.send_message(message.chat.id, "Подтвердите операцию нажав ОК, если оперцию нужно отменить Отмена", reply_markup=markup)
+    bot.register_next_step_handler(result, button_ok)
 
     return
 
-def Button_ok(message):
+def button_ok(message):
     if message.text == 'ОК':
-        bot.send_message(message.chat.id, f"Операция по выгрузке запущена")
-        print("OUT")
-        Start(message)
+        bot.send_message(message.chat.id, "Операция по выгрузке запущена")
+        start(message)
     if message.text == 'ОТМЕНА':
-        bot.send_message(message.chat.id, f"Операция по выгрузке отменена")
+        bot.send_message(message.chat.id, "Операция по выгрузке отменена")
+        start(message)
 
-    # TelegramBot.Start(message)
     return
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#                                BUTTON FOR -url-
 ########################################################################################################################
-@bot.message_handler(content_types=['text'])
-def Website(message):
-    markup = types.InlineKeyboardMarkup(row_width=3)
+########################################################################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''BUTTON URL'''
+########################################################################################################################
+@bot.message_handler(commands=['url'])
+def website(message):
+
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     button_for_bim360 = types.InlineKeyboardButton("BIM360", url=url_BIM360)
     button_for_Google_Sheets = types.InlineKeyboardButton("Google Sheets", url=url_Google_Sheets)
     button_for_Yandex_Disk = types.InlineKeyboardButton("Yandex Disk", url=url_Yandex_Disk)
@@ -315,16 +327,27 @@ def Website(message):
     markup.add(button_for_bim360, button_for_Google_Sheets, button_for_BI_Design, button_for_Yandex_Disk,
                button_for_Google_Docs)
     bot.send_message(message.chat.id, "Пройдите по ссылке снизу:", reply_markup=markup)
+
     return
 ########################################################################################################################
+    # markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    # button_DWG = types.KeyboardButton('DWG')
+    # button_NWC =types.KeyboardButton('NWC')
+    # button_PDF = types.KeyboardButton('PDF')
+    #
+    # markup.add(button_DWG, button_NWC, button_PDF)
+    #
+    # result=bot.send_message(message.chat.id, "Выберите формат для перевода данных:", reply_markup=markup)
+    # bot.register_next_step_handler(result, Callback)
+    #
+    # return
 
 
 
 
-
-#-------------------------------------------OUT ------------------------------------------------------------------------
+#--------------------------------------------------OUT -----------------------------------------------------------------
 
 bot.polling(none_stop=True)
 ########################################################################################################################
 
-#                                          -SAVES-
+#                                                -SAVES-
