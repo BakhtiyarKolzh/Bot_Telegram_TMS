828eda3a765001a388753eb92879a51a6d099818
E.Jumabaev <b.koljabai@taimas-group.kz>
2022-08-16 19:00:32 +0600

    19/00 2022/08/16

--- database.py
+++ database.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
+import time
 import uuid
 import json
-import time, random
+import random
 from collections import OrderedDict
 from multiprocessing import Lock
 
 import RevitSortFiles
+
+rvt_path_list_file = os.path.realpath(r"D:\YandexDisk\RevitExportConfig\revit_path_list_bot.txt")
 
 
 def create_session():
@@ -20,57 +23,38 @@ def add_item_to_dictionary(data, key, value):
         return data
 
 
-def update_item_to_dictionary(data, key, value):
-    if isinstance(value, list):
-        data.update({key: value})
-        return data
-
-
 def pop_item_from_dictionary(data):
     if len(data): return data.popitem(last=False)
 
 
-class ComplexEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, complex):
-            return [obj.real, obj.imag]
-        return json.JSONEncoder.default(self, obj)
-
-
-def remove_file(path):
-    try:
-        if os.path.isfile(path):
-            os.unlink(path)
-    except Exception as e:
-        print(e)
-    return True
-
-
-def write_json_data(path, data, rewrite=True):
-    if rewrite and os.path.exists(path): remove_file(path)
-    if isinstance(data, dict):
+def write_json_data(path, data):
+    if not isinstance(data, dict): return
+    while True:
         try:
+            path = os.path.realpath(path)
             with open(path, "w") as file:
                 json.dump(data, file)
-        except Exception as e:
-            print(e)
-    return True
+                return True
+        except:
+            time.sleep(0.5)
 
 
 def deserialize_json_data(path):
-    if os.path.isfile(path):
-        try:
-            with open(path, "r") as file:
-                return json.load(file)
-        except Exception as e:
-            return print("Error: {}".format(e))
+    while True:
+        if not os.path.isfile(path): return
+        if os.path.isfile(path):
+            try:
+                with open(path, "r") as file:
+                    return json.load(file)
+            except Exception:
+                time.sleep(0.5)
 
 
 def update_json_data(path, data):
     store = deserialize_json_data(path)
     data = (store | data if isinstance(store, dict) else data)
     if isinstance(data, dict):
-        write_json_data(path, data, True)
+        write_json_data(path, data)
         print("Save data count {}\n".format(len(data)))
     return data
 
@@ -92,43 +76,69 @@ def read_command_data(data_path, data):
     with Lock():
         commands = []
         session, action = pop_item_from_dictionary(data)
-        if write_json_data(data_path, data, True):
+        if write_json_data(data_path, data):
             if isinstance(action, list):
                 filepath = action.pop(0)
                 control = action.pop(0)
                 for val in action:
-                    num = None
-                    if isinstance(val, int): num = val
-                    if isinstance(val, str) and val.isdigit(): num = int(val)
-                    if num is not None and num not in commands: commands.append(num)
+                    if isinstance(val, str) and val.isdigit(): val = int(val)
+                    if isinstance(val, int) and val not in commands: commands.append(val)
 
                 return filepath, control, commands
 
 
-def execute_commands(data_path, data):
-    for _ in range(len(data)):
-        result = read_command_data(data_path, data)
-        filepath, control, commands = result
-        paths = RevitSortFiles.get_result_rvt_path_list(filepath)
-        paths = RevitSortFiles.retrieve_paths(paths, commands)
-        if "DWG" == control: print("Set DWG")
-        if "NWC" == control: print("Set NWC")
-        if "IFC" == control: print("Set IFC")
-        if "PDF" == control: print("Set PDF")
-        [print(path) for path in paths]
-        print(commands)
+def run_cmd(control, paths):
+    with Lock():
+        time.sleep(5)
+        global rvt_path_list_file
+        RevitSortFiles.write_revit_path_list_to_file(rvt_path_list_file, paths)
+        if "DWG" == control:
+            bat_file = os.path.realpath(r"D:\YandexDisk\RevitExportConfig\BatFiles\ExportToDWGBot.bat")
+            if os.path.exists(bat_file):
+                os.startfile(bat_file)
+                print("Set DWG")
+        if "NWC" == control:
+            bat_file = os.path.realpath(r"D:\YandexDisk\RevitExportConfig\BatFiles\ExportToNWCBot.bat")
+            if os.path.exists(bat_file):
+                os.startfile(bat_file)
+                print("Set NWC")
+        if "PDF" == control:
+            bat_file = os.path.realpath(r"D:\YandexDisk\RevitExportConfig\BatFiles\ExportToPDFBot.bat")
+            if os.path.exists(bat_file):
+                os.startfile(bat_file)
+                print("Set PDF")
 
-    return print("\n")
+    return
+
+
+def execute_commands(data_path):
+    data = deserialize_json_data(data_path)
+    if isinstance(data, dict):
+        data = OrderedDict(data)
+        for _ in range(len(data)):
+            result = read_command_data(data_path, data)
+            filepath, control, commands = result
+            if isinstance(commands, list) and len(commands):
+                if isinstance(filepath, str) and isinstance(control, str):
+                    commands = [cmd for cmd in commands if isinstance(cmd, int)]
+                    paths = RevitSortFiles.get_result_rvt_path_list(filepath)
+                    paths = RevitSortFiles.retrieve_paths(paths, commands)
+                    [print(path) for path in paths]
+                    run_cmd(control, paths)
+                    print(commands)
+                    print("\n")
+    return
 
 
 database = OrderedDict()
-cmd = ["DWG", "NWC", "IFC", "PDF"]
+cmd = ["DWG", "NWC", "PDF"]
 mypath = os.path.join(os.path.dirname(os.path.realpath(__file__)), "data_file.json")
 
-for x in range(7):
-    time.sleep(0.25)
+for x in range(3):
     control = random.choice(cmd)
     path = r"I:\48_BTG_3-4\01_PROJECT\III_1_AS\01_RVT"
     commands = [str(random.randint(0, 25)) for i in range(5)]
     save_command_data(mypath, path, control, commands)
-    if 5 < x: execute_commands(mypath, database)
+    execute_commands(mypath)
+
+print(rvt_path_list_file)
