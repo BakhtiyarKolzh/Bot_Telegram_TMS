278902339a4d741c4e6681a54c2d09e06485df4b
B.Tokmyrza <b.koljabai@taimas-group.kz>
2022-08-30 20:47:09 +0600

    /30/08/2022

--- asynctelebot.py
+++ asynctelebot.py
@@ -3,35 +3,67 @@
 
 import os
 import os.path
+import time
 
+from pathlib import WindowsPath
 from aiogram import types, executor, Dispatcher, Bot
 
+import database
+import path_manager
+
+import authentication  #### Library for authentication
 import configure  #### Library for Token
 
-#                                       INPUT DATES
+########################################################################################################################
 ########################################################################################################################
 
 bot = Bot(token=(configure.config["token"]))
 dp = Dispatcher(bot)
+users_start = authentication.config["ID"]  # последнее - id группы если бот что-то должен делать в группе
+data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "data_file.json")
 
 flag = False
 directory = None
 controlId = None
 commands = list()
 
+reply_km = types.ReplyKeyboardMarkup
+reply_kb = types.KeyboardButton
+inline_km = types.InlineKeyboardMarkup
+inline_kb = types.InlineKeyboardButton
+
 ########################################################################################################################
 ########################################################################################################################
+
+'''   BOT PROTECTION    '''
+
+
+@dp.message_handler(lambda message: message.chat.id not in users_start, commands=['start'])
+async def protection_id(message: types.Message):
+    await message.answer('У Вас нет прав на выполнение данной команды, обратитесь к администратору')
+
+
+########################################################################################################################
+########################################################################################################################
+
 '''   START TELEGRAM    '''
 
 
 @dp.message_handler(commands=['start'])
 async def command_start(message: types.Message):
+    global commands
     global controlId
+    global directory
+    global controlId
+    global reply_km
+    global reply_kb
     chat_id = message.from_user.id
+    commands, controlId, directory = list(), None, None
+
     welcome = f"Добро пожаловать, <b>{message.from_user.first_name}</b>"
     await bot.send_message(chat_id=chat_id, text=welcome, parse_mode='html')
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    markup.add(types.KeyboardButton(text='Начать задание'))
+    markup = reply_km(resize_keyboard=True)
+    markup.add(reply_kb(text='Начать задание'))
     await message.answer("Выберите команду Начать задание", reply_markup=markup)
     print('Начать задание')
 
@@ -39,18 +71,20 @@ async def command_start(message: types.Message):
 ########################################################################################################################
 ########################################################################################################################
 
+''' НАЧАТЬ ЗАДАНИЕ '''
+
+
 @dp.message_handler(lambda message: message.text == 'Начать задание')
 async def call_back_start_to_format(message: types.Message):
     global flag
+    global reply_km
+    global reply_kb
     flag = True
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    markup.add(types.KeyboardButton(text='DWG'), types.KeyboardButton(text='NWC'), types.KeyboardButton(text='PDF'))
+    markup = reply_km(resize_keyboard=True)
+    markup.add(reply_kb(text='DWG'), reply_kb(text='NWC'), reply_kb(text='PDF'))
     await message.answer("Выберите формат для перевода данных:", reply_markup=markup)
     print("Выберите формат для перевода данных:")
 
-
-########################################################################################################################
-########################################################################################################################
 
 @dp.message_handler(lambda message: message.text in ['DWG', 'NWC', 'PDF'])
 async def call_back_format(message: types.Message):
@@ -61,208 +95,135 @@ async def call_back_format(message: types.Message):
         global controlId
         controlId = message.text
         print(controlId)
-        flag = False
 
 
 ########################################################################################################################
 ########################################################################################################################
 
-# @dp.message_handler(lambda message: message.text in ['DWG', 'NWC', 'PDF'])
-# async def check_directory(message: types.Message):
-#     global directory
-#     global flag
-#
-#     directory = os.path.realpath(message.text)
-#     if os.path.exists(directory):
-#         await menu_for_button(message)
-#         print("exist")
-#
-
-########################################################################################################################
-########################################################################################################################
-
-'''Menu_for_button'''
+'''ВЫБОР ФАЙЛОВ, ВЫБРАТЬ ВСЕ ФАЙЛЫ, ОТОЛОЖИТЬ ОПЕРАЦИЮ '''
 
 
 @dp.message_handler(lambda message: os.path.exists(os.path.realpath(message.text)))
 async def menu_for_button(message: types.Message):
+    global flag
+    global reply_km
+    global reply_kb
     print(message.text)
-
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    markup.add((types.KeyboardButton(text='Выбор файлов')), (types.KeyboardButton(text='Выбрать все файлы')),
-               (types.KeyboardButton(text='Отложить операцию')))
-    await message.answer("Выберите нужную операцию", reply_markup=markup)
-    print("Выберите нужную операцию")
-    await call_back_menu(message)
+    if flag:
+        markup = reply_km(resize_keyboard=True)
+        markup.add((reply_kb(text='Выбор файлов')), (reply_kb(text='Выбрать все файлы')),
+                   (reply_kb(text='Отложить операцию')))
+        await message.answer("Выберите нужную операцию", reply_markup=markup)
+        print("Выберите нужную операцию")
+        await call_back_menu(message)
 
 
 @dp.message_handler(lambda message: len(message.text) > 15 and not os.path.exists(os.path.realpath(message.text)))
 async def test_for_button(message: types.Message):
-    await message.answer("okey")
+    # await menu_button_ok_and_cancel(message)
+    await message.answer("MISTAKE")
     print(message.text)
 
 
-########################################################################################################################
-########################################################################################################################
-
 @dp.message_handler(lambda message: message.text in ['Выбор файлов', 'Выбрать все файлы', 'Отложить операцию'])
 async def call_back_menu(message: types.Message):
-    if message.text == "Выбор файлов":
-        print("Выбор файлов")
-    elif message.text == "Выбрать все файлы":
-        print("Выбрать все файлы")
-    elif message.text == "Отложить операцию":
-        print("ОТМЕНА")
+    msg = message.text
+    global flag
+    if flag:
+        if msg == "Выбор файлов":
+            print("Выбор файлов")
+            await menu_button_ok_and_cancel(message)
+            # await cmd_select_inline(message, directory)
+            # await menu_button_ok_and_cancel(message)
+
+        elif msg == "Выбрать все файлы":
+            print("Выбрать все файлы")
+            # await menu_button_ok_and_cancel(message)
+
+        elif msg == "Отложить операцию":
+            print("Отложить операцию")
+            # await menu_button_ok_and_cancel(message)
 
 
 ########################################################################################################################
 ########################################################################################################################
 
+'''SELECT A SECTION --- def'''
 
-# @dp.callback_query_handler(text='DWG')
-# @dp.callback_query_handler(text='NWC')
-# @dp.callback_query_handler(text='PDF')
-# # @dp.message_handler(content_types=['text'], regexp=r"^(\w+)")
-# async def call_button_batch(message: types.Message):
-#     global controlId
-#     chat_id = message.from_user.id
-#     if message.text == "Начать задание":
-#         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-#         markup.add(types.KeyboardButton('DWG'), types.KeyboardButton('NWC'), types.KeyboardButton('PDF'))
-#         await bot.send_message(chat_id, "Выберите формат для перевода данных:", reply_markup=markup)
-#         print("Выберите формат для перевода данных:")
-#
-#     if message.text in ['DWG', 'NWC', 'PDF']:
-#         print('format')
-#         print('it goes to the path ')
-#         await bot.send_message(chat_id, "Введите путь")
-#         if message.text == "DWG":
-#             controlId = "DWG"
-#             print(controlId)
-#             await user_answer_format(message)
-#         if message.text == "NWC":
-#             controlId = "NWC"
-#             print(controlId)
-#             await user_answer_format(message)
-#         if message.text == "PDF":
-#             controlId = "PDF"
-#             print(controlId)
-#             await user_answer_format(message)
-#     else:
-#         await asyncio.
-#         await call_button_batch(message)
-#     return
+hide = types.InlineKeyboardButton
 
-########################################################################################################################
-########################################################################################################################
-#
-# @dp.message_handler(content_types=['text'])
-# async def user_answer_format(message: types.Message):
-#     global directory
-#     directory = os.path.realpath(message.text)
-#     if os.path.exists(directory):
-#         await menu_for_button(message)
-#         print("exist")
-#
-#     return
-#
-#
-# ########################################################################################################################
-# ########################################################################################################################
-#
-# '''Menu_for_button'''
-#
-#
-# @dp.message_handler(content_types=['text'])
-# async def menu_for_button(message: types.Message):
-#     global commands
-#     chat_id = message.from_user.id
-#     if message.text:
-#         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-#         markup.add(types.KeyboardButton('Выбрать все файлы'), types.KeyboardButton('Выбрать все файлы'),
-#                    types.KeyboardButton('ОТМЕНА'))
-#         await bot.send_message(chat_id, "Выберите нужную операцию", reply_markup=markup)
-#         print("Выберите нужную операцию")
-#     if message.text == "Выбор файлов" or "Выбрать все файлы" or "ОТМЕНА":
-#         print('f11111')
-#         if message.text == "Выбор файлов":
-#             print("Выбор файлов")
-#
-#         elif message.text == "Выбрать все файлы":
-#             print("Выбрать все файлы")
-#
-#         elif message.text == "ОТМЕНА":
-#             print("ОТМЕНА")
-#
-#         return
-#     return
-#
-#
-# '''SELECT A SECTION --- def'''
+@dp.message_handler(lambda message: message.text in ['Выбор файлов', 'Выбрать все файлы', 'Отложить операцию'])
+async def cmd_select_inline(message: types.Message, project_path):
+    buttons = []
+    time.sleep(0.5)
+    keyboard = inline_km(row_width=1)
+    paths = path_manager.get_result_rvt_path_list(project_path)
+    for idx, path in enumerate(paths):
+        name, ext = os.path.splitext(WindowsPath(path).name)
+        buttons.append(inline_kb(name, callback_data=idx + 1 or 'btn'))
 
 
-# hide = types.InlineKeyboardButton
-#
-# @dp.message_handler(content_types=['text'])
-# async def cmd_select_inline(message: types.Message, project_path):
-#     chat_id = message.from_user.id
-#     buttons = []
-#     time.sleep(0.5)
-#     keyboard = types.InlineKeyboardMarkup(row_width=1)
-#     paths = path_manager.get_result_rvt_path_list(project_path)
-#     for idx, path in enumerate(paths):
-#         name, ext = os.path.splitext(WindowsPath(path).name)
-#         buttons.append(types.InlineKeyboardButton(name, callback_data=idx + 1))
-#
-#     keyboard.add(*buttons)
-#     await bot.send_message(chat_id, "Выбрать: ", reply_markup=keyboard)
-#     await call_button_ok_and_cancel(message)
-#
-#     return
+    keyboard.add(*buttons)
+    await message.answer("Выбрать: ", reply_markup=keyboard)
+    await menu_button_ok_and_cancel(message)
 
-# @dp.callback_query_handler(func=lambda call: True)
-# def call_for_cmd_line(call):
-#     global commands
-#     if any(call.data):
-#         number = call.data
-#         number = int(number) if number.isdigit() else 0
-#         commands.append(number)
-#         print(number)
-#
-#     return
+
+@dp.callback_query_handler(lambda c: c.data == 'btn')
+async def call_for_cmd_line(callback_query: types.CallbackQuery):
+    global commands
+    if any(call.data):
+        number = call.data
+        number = int(number) if number.isdigit() else 0
+        commands.append(number)
+        print(number)
+
+
+
+
 
 
 ########################################################################################################################
 ########################################################################################################################
-# @dp.message_handler(content_types=['text'])
-# async def call_button_ok_and_cancel(message):
-#     global commands
-#     global controlId
-#     global directory
-#     chat_id = message.from_user.id
-#     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-#     markup.add(types.KeyboardButton('ОК'), types.KeyboardButton('ОТМЕНА'))
-#     markup.one_time_keyboard = True
-#     await bot.send_message(chat_id, "Введите данные", reply_markup=markup)
-#     msg = message.text
-#     print(msg)
-#
-#     if msg == 'ОК':
-#         print('ОК')
-#         return True
-#
-#     elif msg == 'ОТМЕНА':
-#         print('ОТМЕНА')
-#         return True
-#
-#
-# ########################################################################################################################
-########################################################################################################################
+
+'''BUTTONS OK AND OTMENA'''
+
+
+@dp.message_handler(lambda message: message.text in ['Выбор файлов', 'Выбрать все файлы', 'Отложить операцию'])
+async def menu_button_ok_and_cancel(message):
+    global commands
+    global controlId
+    global directory
+    global reply_km
+    global reply_kb
+    global flag
+    if flag:
+        markup = reply_km(resize_keyboard=True)
+        markup.add(reply_kb('ОК'), reply_kb('ОТМЕНА'))
+        markup.one_time_keyboard = True
+
+        await message.answer("Подтвердите выбранную операцию", reply_markup=markup)
+        await call_back_ok_and_cancel(message)
+
+
+@dp.message_handler(lambda message: message.text in ['ОК', 'ОТМЕНА'])
+async def call_back_ok_and_cancel(message: types.Message):
+    msg = message.text
+    global flag
+    if flag:
+        if msg == 'ОК':
+            print('ОК')
+            await command_start(message)
+            return True
+
+        elif msg == 'ОТМЕНА':
+            print('ОТМЕНА')
+            await command_start(message)
+            return True
 
 
 ########################################################################################################################
 ########################################################################################################################
-#
+# #
 # async def is_enabled():
 #     print('запускаю цикл')
 #     while True:
@@ -270,18 +231,21 @@ async def call_back_menu(message: types.Message):
 #         for user_id in range(100):
 #             await asyncio.sleep(1)
 #             print('отправил')
+#             await asyncio.sleep(1)
+#             print('1111')
 #         print('жду')
 #         await asyncio.sleep(10)
 #
 #
+#
 # async def on_startup(x):
 #     asyncio.create_task(is_enabled())
-#
-#
-#
+
+
 # if __name__ == '__main__':
 #     executor.start_polling(dp, skip_updates=True, timeout=1, on_startup=on_startup)
 
+# --------------------------------------------------OUT ----------------------------------------------------------------
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=False, timeout=1)
--- temp.py
+++ temp.py
@@ -282,3 +282,144 @@
 #             print(controlId)
 #         #
 #         # else:
+
+
+# @dp.callback_query_handler(text='DWG')
+# @dp.callback_query_handler(text='NWC')
+# @dp.callback_query_handler(text='PDF')
+# # @dp.message_handler(content_types=['text'], regexp=r"^(\w+)")
+# async def call_button_batch(message: types.Message):
+#     global controlId
+#     chat_id = message.from_user.id
+#     if message.text == "Начать задание":
+#         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+#         markup.add(types.KeyboardButton('DWG'), types.KeyboardButton('NWC'), types.KeyboardButton('PDF'))
+#         await bot.send_message(chat_id, "Выберите формат для перевода данных:", reply_markup=markup)
+#         print("Выберите формат для перевода данных:")
+#
+#     if message.text in ['DWG', 'NWC', 'PDF']:
+#         print('format')
+#         print('it goes to the path ')
+#         await bot.send_message(chat_id, "Введите путь")
+#         if message.text == "DWG":
+#             controlId = "DWG"
+#             print(controlId)
+#             await user_answer_format(message)
+#         if message.text == "NWC":
+#             controlId = "NWC"
+#             print(controlId)
+#             await user_answer_format(message)
+#         if message.text == "PDF":
+#             controlId = "PDF"
+#             print(controlId)
+#             await user_answer_format(message)
+#     else:
+#         await asyncio.
+#         await call_button_batch(message)
+#     return
+
+########################################################################################################################
+########################################################################################################################
+#
+# @dp.message_handler(content_types=['text'])
+# async def user_answer_format(message: types.Message):
+#     global directory
+#     directory = os.path.realpath(message.text)
+#     if os.path.exists(directory):
+#         await menu_for_button(message)
+#         print("exist")
+#
+#     return
+#
+#
+# ########################################################################################################################
+# ########################################################################################################################
+#
+# '''Menu_for_button'''
+#
+#
+# @dp.message_handler(content_types=['text'])
+# async def menu_for_button(message: types.Message):
+#     global commands
+#     chat_id = message.from_user.id
+#     if message.text:
+#         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+#         markup.add(types.KeyboardButton('Выбрать все файлы'), types.KeyboardButton('Выбрать все файлы'),
+#                    types.KeyboardButton('ОТМЕНА'))
+#         await bot.send_message(chat_id, "Выберите нужную операцию", reply_markup=markup)
+#         print("Выберите нужную операцию")
+#     if message.text == "Выбор файлов" or "Выбрать все файлы" or "ОТМЕНА":
+#         print('f11111')
+#         if message.text == "Выбор файлов":
+#             print("Выбор файлов")
+#
+#         elif message.text == "Выбрать все файлы":
+#             print("Выбрать все файлы")
+#
+#         elif message.text == "ОТМЕНА":
+#             print("ОТМЕНА")
+#
+#         return
+#     return
+#
+#
+# '''SELECT A SECTION --- def'''
+
+
+# hide = types.InlineKeyboardButton
+#
+# @dp.message_handler(content_types=['text'])
+# async def cmd_select_inline(message: types.Message, project_path):
+#     chat_id = message.from_user.id
+#     buttons = []
+#     time.sleep(0.5)
+#     keyboard = types.InlineKeyboardMarkup(row_width=1)
+#     paths = path_manager.get_result_rvt_path_list(project_path)
+#     for idx, path in enumerate(paths):
+#         name, ext = os.path.splitext(WindowsPath(path).name)
+#         buttons.append(types.InlineKeyboardButton(name, callback_data=idx + 1))
+#
+#     keyboard.add(*buttons)
+#     await bot.send_message(chat_id, "Выбрать: ", reply_markup=keyboard)
+#     await call_button_ok_and_cancel(message)
+#
+#     return
+
+# @dp.callback_query_handler(func=lambda call: True)
+# def call_for_cmd_line(call):
+#     global commands
+#     if any(call.data):
+#         number = call.data
+#         number = int(number) if number.isdigit() else 0
+#         commands.append(number)
+#         print(number)
+#
+#     return
+
+
+########################################################################################################################
+########################################################################################################################
+# @dp.message_handler(content_types=['text'])
+# async def call_button_ok_and_cancel(message):
+#     global commands
+#     global controlId
+#     global directory
+#     chat_id = message.from_user.id
+#     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+#     markup.add(types.KeyboardButton('ОК'), types.KeyboardButton('ОТМЕНА'))
+#     markup.one_time_keyboard = True
+#     await bot.send_message(chat_id, "Введите данные", reply_markup=markup)
+#     msg = message.text
+#     print(msg)
+#
+#     if msg == 'ОК':
+#         print('ОК')
+#         return True
+#
+#     elif msg == 'ОТМЕНА':
+#         print('ОТМЕНА')
+#         return True
+#
+#
+# ########################################################################################################################
+########################################################################################################################
